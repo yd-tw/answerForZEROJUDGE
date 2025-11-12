0b9d93a160ad2729035733df771f8d345b1785d8
singhone0515 <singhone0515@gmail.com>
2025-11-12 22:07:35 +0800

    fix: vercel entrypoint for express

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 
 admin.initializeApp({
-    credential: admin.credential.cert(key)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
