d7087a3d8b67039ae67c3de822b8b8f21fa73116
singhone0515 <singhone0515@gmail.com>
2025-11-12 17:56:46 +0800

    fix: vercel entrypoint for express

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
+// ✅ index.js — 給 Vercel 當入口用
 import express from "express";
-import app from "./server.js";
+import appHandler from "./server.js";
 
+const app = express();
+
+// 使用 server.js 裡的設定掛載
+app.use(appHandler);
+
+// ✅ 讓 Vercel 偵測到 express 並導出 app
 export default app;
